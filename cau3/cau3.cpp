4b3815f5585d5d3a0e0bb46e9b7a4a7b7c9c92ff
tluan1 <luanthanh253@gmail.com>
2024-02-26 08:46:07 +0700

    Add project files.

---
+++ cau3/cau3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b, tich;
+	cout << "chuong trinh tinh tich\n";
+	cout << "nhap a:"; cin >> a;
+	cout << "nhap b:"; cin >> b;
+	tich = a * b;
+	cout << a<< "*" << b<< "=" << tich<< endl;
+	return 0;
+}
+
